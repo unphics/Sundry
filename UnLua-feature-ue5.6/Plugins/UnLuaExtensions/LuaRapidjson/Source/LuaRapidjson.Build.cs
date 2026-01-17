05a69a92a06d2552cd64e6b0b91730f2be1d9216
unphics <481381521@qq.com>
2026-01-17 19:16:25 +0800

    UnLua4UE5.6

---
+++ UnLua-feature-ue5.6/Plugins/UnLuaExtensions/LuaRapidjson/Source/LuaRapidjson.Build.cs
@@ -0,0 +1,64 @@
+// Tencent is pleased to support the open source community by making UnLua available.
+// 
+// Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Licensed under the MIT License (the "License"); 
+// you may not use this file except in compliance with the License. You may obtain a copy of the License at
+//
+// http://opensource.org/licenses/MIT
+//
+// Unless required by applicable law or agreed to in writing, 
+// software distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and limitations under the License.
+
+using System;
+using System.IO;
+using UnrealBuildTool;
+
+public class LuaRapidjson : ModuleRules
+{
+    public LuaRapidjson(ReadOnlyTargetRules Target) : base(Target)
+    {
+#if UE_5_2_OR_LATER
+        IWYUSupport = IWYUSupport.None;
+#else
+        bEnforceIWYU = false;
+#endif
+        bUseUnity = false;
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+#if UE_5_6_OR_LATER
+        CppCompileWarningSettings.UndefinedIdentifierWarningLevel = WarningLevel.Off;
+#elif UE_5_5_OR_LATER
+		UndefinedIdentifierWarningLevel = WarningLevel.Off;
+#else
+        bEnableUndefinedIdentifierWarnings = false;
+#endif
+        bEnableExceptions = true;
+
+        PublicDependencyModuleNames.AddRange(
+            new[]
+            {
+                "Core",
+                "CoreUObject",
+                "Engine",
+                "InputCore",
+            });
+
+        PrivateDependencyModuleNames.AddRange(new[]
+        {
+            "UnLua",
+            "Lua"
+        });
+
+        PrivateDefinitions.AddRange(
+            new[]
+            {
+                "LUA_LIB"
+            }
+        );
+        
+        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "src"));
+        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "include"));
+    }
+}
