4fc26944ee97ddf0ee58005fd4a6b8a9d6e69586
unphics <481381521@qq.com>
2026-02-07 19:33:54 +0800

    1

--- imgui_sdl_vk/README.md
+++ imgui_sdl_vk/README.md
@@ -6,5 +6,9 @@ imgui_sdl_vk/
 ├── imgui/                  # dear_imgui源码
 ├── lib/                    # 存放sdl2/vulkan等库
 ├── src/                    # 工程源代码
+|   ├── private/            # 工程的源文件
+|   └── public/             # 工程的头文件
+|       ├── sdl2/           # imgui中对sdl的封装文件
+|       └── vulkan/         # imgui中对vulkan的封装文件
 └── xmake.lua               # xmake构建脚本
 ````
--- imgui_sdl_vk/xmake.lua
+++ imgui_sdl_vk/xmake.lua
@@ -1,13 +1,28 @@
 add_rules("mode.debug", "mode.release")
 
-add_requires("vulkansdk", {system=true })
-add_requires("libsdl2")
+-- 1. 依赖包管理
+if is_plat("android") then
+    add_require("libsdl2")
+else
+    -- windows
+    add_requires("vulkansdk", {system=true })
+    add_requires("libsdl2")
+end
+
 
 target("imgui_sdl_vk")
-    set_kind("binary")
-    set_targetdir('bin')
-    -- 应用依赖包
+    -- 2. 根据平台设置生成的类型
+    if is_plat("android") then
+        set_kind("shared")
+    else
+        set_kind("binary")
+        set_targetdir('bin')
+    end
+
+    -- 3. 应用依赖包
     add_packages("vulkansdk", "libsdl2")
+
+    -- 4. 自动处理sdl2路径
     -- 因为 ImGui 代码里通常写的是 #include <SDL.h> 
     -- 而包管理器下载的路径可能是 include/SDL2/SDL.h
     after_load(function (target)
@@ -20,12 +35,24 @@ target("imgui_sdl_vk")
             end
         end
     end)
-    -- imgui配置
+
+    -- 5.头文件路径
     add_includedirs("imgui")
-    add_files("imgui/*.cpp")
-    -- 项目源码
     add_includedirs("src")
     add_includedirs("src/public")
+
+    -- 6.源文件路径
+    add_files("imgui/*.cpp")
     add_files("src/**.cpp")
+    add_files("src/**.cc")
     -- 如果imgui需要sdl和vulkan的backend实现文件
+
+    -- 7. 安卓平台特有的系统库链接和配置
+    if is_plat("android") then
+        -- log: 打印日志到Logcat; android: 安卓核心库; vulkan: 核心渲染库
+        add_links("android", "log", "vulkan")
+
+        -- 解决NDK入口函数问题
+        add_defines("SDL_MAIN_HANDLED")
+    end
 target_end()
